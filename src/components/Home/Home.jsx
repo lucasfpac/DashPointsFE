43b61a80539c515a90572fc620ef71e323f4c142
Murilo Pontes <murilopjr@gmail.com>
2024-09-18 08:36:17 -0300

    mudanca no estilo da Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from '../../assets/img/logo.png';
-import { Button } from '../ui/button';
-import { Link } from 'react-router-dom';
-import Footer from '../Footer/Footer';
+import React from "react";
+import logo from "../../assets/img/logo.png";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
-    <section className="flex flex-col min-h-screen bg-black">
-      <div className="flex flex-col justify-center items-center flex-grow">
-        <div className="flex flex-col items-center justify-center text-white font-bold">
-          <div className="w-[500px] max-w-full">
-            <img src={logo} alt="Logo" />
+    <section className='flex flex-col min-h-screen bg-black'>
+      <div className='flex flex-col justify-center items-center flex-grow'>
+        <div className='flex flex-col items-center justify-center text-white font-bold'>
+          <div className='w-[500px] max-w-full'>
+            <img src={logo} alt='Logo' />
           </div>
           <p>Seja bem-vindo!</p>
           <p>
@@ -18,12 +18,16 @@ const Home = () => {
             poucos cliques
           </p>
         </div>
-        <div className="flex flex-col gap-5 mt-5">
-          <Link to="/cadastro">
-            <Button variant="outline">CADASTRAR CLIENTE</Button>
+        <div className='flex flex-col gap-5 mt-5 w-[300px] max-w-full'>
+          <Link to='/cadastro'>
+            <Button variant='outline' className='w-full'>
+              CADASTRAR CLIENTE
+            </Button>
           </Link>
-          <Link to="/busca">
-            <Button variant="outline">BUSCAR CLIENTE</Button>
+          <Link to='/busca'>
+            <Button variant='outline' className='w-full'>
+              BUSCAR CLIENTE
+            </Button>
           </Link>
         </div>
       </div>
