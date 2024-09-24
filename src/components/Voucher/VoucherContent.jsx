e6944a6c944c345f90cd87a76fadad432f832e3a
Murilo Pontes <murilopjr@gmail.com>
2024-09-24 11:01:23 -0300

    mudanca no voucher

--- src/components/Voucher/VoucherContent.jsx
+++ src/components/Voucher/VoucherContent.jsx
@@ -13,33 +13,50 @@ const VoucherContent = ({ purchase, customer }) => {
   }
 
   return (
-    <div>
-      <h1>VOUCHER PARA TROCA</h1>
-      <p>Emitido em {formatDate(purchase.date) || "Data não disponível"}</p>
-      <hr />
-      <div className='section'>
-        <p>
-          <strong>Dados Pessoais:</strong>
+    <div class='voucher'>
+      <div class='voucher-info'>
+        <p class='centered-bold'>VOUCHER PARA TROCA</p>
+        <p class='centered'>
+          Emitido em {formatDate(purchase.date) || "Data não disponível"}
         </p>
-        <p>CPF/CNPJ: {formatCPFOrCNPJ(customer.cpf_cnpj)}</p>
-        <p>Nome: {customer.full_name}</p>
-        <p>E-mail: {customer.email}</p>
       </div>
-      <hr />
-      <div className='section'>
+      <div class='separator'></div>
+      <div class='voucher-personal-info'>
+        <p class='centered-bold'>Dados Pessoais</p>
+        <br />
         <p>
-          <strong>Dados da Compra:</strong>
+          <strong>CPF/CNPJ:</strong> {formatCPFOrCNPJ(customer.cpf_cnpj)}
         </p>
-        <p>Loja: {purchase.store_name}</p>
-        <p>Número do pedido / NF: {purchase.invoice}</p>
-        <p>Valor da Nf: {formatCurrency(purchase.value)}</p>
+        <p>
+          <strong>Nome:</strong> {customer.full_name}
+        </p>
+        <p>
+          <strong>E-mail:</strong> {customer.email}
+        </p>
       </div>
-      <hr />
-      <p>
-        <strong>Valor total: {formatCurrency(purchase.value)}</strong>
-      </p>
-      <p>Procure o espaço concierge e informe este</p>
-      <div className='center'>
+      <div class='separator'></div>
+      <div class='voucher-purchase-info'>
+        <p class='centered-bold'>Dados da Compra</p>
+        <br />
+        <p>
+          <strong>Loja:</strong> {purchase.store_name}
+        </p>
+        <p>
+          <strong>Número do pedido / NF:</strong> {purchase.invoice}
+        </p>
+        <p>
+          <strong>Valor da NF:</strong> {formatCurrency(purchase.value)}
+        </p>
+      </div>
+      <div class='separator'></div>
+      <div class='voucher-footer'>
+        <p>
+          <strong>Valor Total:</strong> {formatCurrency(purchase.value)}
+        </p>
+        <br />
+        <p>Procure o espaço concierge e informe este</p>
+      </div>
+      <div class='voucher-confirmation-code'>
         <p>CÓDIGO DE CONFIRMAÇÃO</p>
         <p>{purchase.id}</p>
       </div>
--- src/components/Voucher/VoucherPrintLayout.jsx
+++ src/components/Voucher/VoucherPrintLayout.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import VoucherContent from './VoucherContent';
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import VoucherContent from "./VoucherContent";
 
 const PrintLayout = ({ purchase, customer }) => {
   const purchaseHTML = ReactDOMServer.renderToString(
@@ -8,41 +8,68 @@ const PrintLayout = ({ purchase, customer }) => {
   );
 
   return `
-    <html>
+    <!DOCTYPE html>
+      <html lang="pt-br">
       <head>
-        <title>VOUCHER PARA TROCA</title>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-            width: 62mm;
-            height: 100mm;
-            padding: 10px;
-            margin: 0;
-            box-sizing: border-box;
-          }
-          h1 {
-            font-size: 16px;
-            text-align: center;
-            margin-bottom: 5px;
-          }
-          hr {
-            margin: 10px 0;
-            border: 1px solid #000;
-          }
-          .section {
-            margin-bottom: 10px;
-          }
-          p {
-            font-size: 14px;
-            margin: 2px 0;
-          }
-          .center {
-            text-align: center;
-            font-size: 16px;
-            font-weight: bold;
-            margin-top: 20px;
-          }
-        </style>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Voucher</title>
+          <style>
+              
+              @media print {
+                  body {
+                      margin: 0;
+                      padding: 0;
+                  }
+              }
+
+              .voucher {
+                  width: 62mm;
+                  height: 100mm;
+                  padding: 5px;
+                  box-sizing: border-box;
+                  font-family: Arial, sans-serif;
+                  border: 1px dashed #333;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-between;
+              }
+
+              .voucher-info, .voucher-personal-info, .voucher-purchase-info, .voucher-footer {
+                  margin: 5px 0;
+                  font-size: 10px;
+                  color: #333;
+              }
+
+              .voucher-info p, .voucher-personal-info p, .voucher-purchase-info p, .voucher-footer p {
+                  margin: 2px 0;
+              }
+
+              .separator {
+                  margin: 5px 0;
+                  border-bottom: 1px solid #000;
+              }
+
+              .voucher-confirmation-code {
+                  text-align: center;
+                  margin-top: 5px;
+              }
+
+              .voucher-confirmation-code p {
+                  font-size: 12px;
+                  font-weight: bold;
+                  color: #333;
+              }
+
+              .centered-bold {
+                  text-align: center;
+                  font-weight: bold;
+              }
+
+              .centered {
+                  text-align: center;
+              }
+          </style>
       </head>
       <body>
         ${purchaseHTML}
